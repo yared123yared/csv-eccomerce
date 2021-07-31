576709d2ac8f7c514a0ef56d934dc7dd180f9cba
yared123yared <yaredyaya16@gmail.com>
2021-07-31 11:53:27 -0700

    added reset password, new credentials and verificaiton screen

--- app/lib/screens/home_screen.dart
+++ app/lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'profile_screen.dart';
-import '../constants.dart';
+import '../constants/constants.dart';
 
 final _scaffoldKey = GlobalKey<ScaffoldState>();
 
