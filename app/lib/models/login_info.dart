8cc406a036d33a7951a81d4f08b4e4be33323eb5
yared123yared <yaredyaya16@gmail.com>
2021-08-01 13:05:12 -0700

    updated structure of the code

--- app/lib/models/login_info.dart
+++ app/lib/models/login_info.dart
@@ -11,12 +11,12 @@ class LoggedUserInfo {
   User? user;
 
   LoggedUserInfo({this.token, this.user});
-
+  // read from json
   LoggedUserInfo.fromJson(Map<String, dynamic> json) {
     token = json['token'];
     user = json['user'] != null ? new User.fromJson(json['user']) : null;
   }
-
+// write from json
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['token'] = this.token;
