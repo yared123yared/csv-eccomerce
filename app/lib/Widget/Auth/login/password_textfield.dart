1d648bdbc3ac23c67a97a738c4bb63d09b1eaa9b
yared123yared <yaredyaya16@gmail.com>
2021-08-02 10:44:15 -0700

    updated the login to be scrollable

--- app/lib/Widget/Auth/login/password_textfield.dart
+++ app/lib/Widget/Auth/login/password_textfield.dart
@@ -1,17 +1,22 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// import '../Common/custom_textfield.dart';
+import '../Common/custom_textfield.dart';
 
-// class PasswordTextField extends StatelessWidget {
-//   final passwordEditingController;
-//   PasswordTextField({this.passwordEditingController});
-//   @override
-//   Widget build(BuildContext context) {
-//     // final _mypasswordController = TextEditingController();
-//     return CustomTextField(
-//       textFieldName: "Enter Passowrd",
-//       controller: passwordEditingController,
-//       icon: Icon(Icons.lock, color: Colors.black),
-//     );
-//   }
-// }
+class PasswordTextField extends StatelessWidget {
+  final passwordEditingController;
+  PasswordTextField({this.passwordEditingController});
+  @override
+  Widget build(BuildContext context) {
+    // final _mypasswordController = TextEditingController();
+    return CustomTextField(
+        textFieldName: "Enter Passowrd",
+        controller: passwordEditingController,
+        icon: Icons.lock,
+        obsecureText: false,
+        validator: (value) {
+          if (value == null) {
+            return "Password is empty";
+          }
+        });
+  }
+}
