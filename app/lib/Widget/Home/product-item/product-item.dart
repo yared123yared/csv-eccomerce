9e120165789f2fe80fb766467272dbbb95ff42cd
Alefew Yimer <alefewdev21@gmail.com>
2021-08-29 23:56:54 +0300

    cache image and fix overflow

--- app/lib/Widget/Home/product-item/product-item.dart
+++ app/lib/Widget/Home/product-item/product-item.dart
@@ -5,10 +5,10 @@ import 'package:app/models/product/data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'conditional.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+
 
 class ProductItem extends StatefulWidget {
-  Data product;
+  final Data product;
 
   // final VoidCallback onPressed;
 
